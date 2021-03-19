34fbaba9c475e4423df61acd8a24e486653305b9
pi_droneci <tcardoso@gmail.com>
2021-03-19 14:28:59 +0800

    First commit

---
+++ docker_start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker run -d --name gitlab-runner --restart always \
+     -v /srv/gitlab-runner/config:/etc/gitlab-runner \
+     -v /var/run/docker.sock:/var/run/docker.sock \
+     gitlab/gitlab-runner:latest
